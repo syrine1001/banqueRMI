071cc6b1bab68f672d623302a16f685fa6aa8874
syrine1001 <76111401+syrine1001@users.noreply.github.com>
2020-12-26 11:33:31 +0100

    Update Compte.java

--- banque/Compte.java
+++ banque/Compte.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package banque;
 
-/**
- *
- * @author Syrine
- */
 public class Compte implements java.io.Serializable{
        int numero;
     double solde;
